bbd5948787605d24873e0ee03881cf6f2b3ced1f
Obi Uchenna David <daviduchenna@outlook.com>
2017-07-08 22:57:02 +0100

    Created user model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class User(AbstractUser):
+    name = models.CharField(_('Name of User'), blank=True, max_length=255)
+
+    def __str__(self):
+        return self.username
