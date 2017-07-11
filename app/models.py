1effe81b2e61f1f6e94b51b16228a309cb932fd0
Obi Uchenna David <daviduchenna@outlook.com>
2017-07-11 23:31:21 +0100

    created PrintJob model

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from model_utils.models import TimeStampedModel
 
 
 class User(AbstractUser):
@@ -10,3 +11,20 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Client(User):
+    pass
+
+
+class Document(models.Model):
+    pass
+
+
+class PrintJob(TimeStampedModel):
+    charged_to = models.ForeignKey(Client)
+    printer = models.CharField(max_length=256)
+    copies = models.IntegerField()
+    cost = models.IntegerField()
+    status = models.CharField()
+    document = models.ForeignKey(Document)
