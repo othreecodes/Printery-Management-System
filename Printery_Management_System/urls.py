2bcbce7c2562f3a0a365fd520a02c8394edc3cfc
Obi Uchenna David <daviduchenna@outlook.com>
2017-07-28 20:43:43 +0100

    added static files and media  urls

--- Printery_Management_System/urls.py
+++ Printery_Management_System/urls.py
@@ -13,9 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url ,include
 from django.contrib import admin
+from . import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
+    url(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
     url(r'^admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "UI Printery Mangement System"
+admin.site.site_title =  "UI Printery Mangement System"
+
+urlpatterns += staticfiles_urlpatterns()
+
+
