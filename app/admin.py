8a01d3e41ce0b67c62972c737761b4001668e13b
Obi Uchenna David <daviduchenna@outlook.com>
2017-07-28 20:36:40 +0100

    registered admins

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
+"""
+Contains all registered admin views
+"""
 from django.contrib import admin
+from app.models import (User, Client, Document, PrintJob)
 
-# Register your models here.
+
+admin.site.register(User)
+admin.site.register(Client)
+admin.site.register(Document)
+admin.site.register(PrintJob)
--- app/models.py
+++ app/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from model_utils.models import TimeStampedModel
 
 
 class User(AbstractUser):
+    """
+    User Model
+    """
     name = models.CharField(_('Name of User'), blank=True, max_length=255)
 
     def __str__(self):
@@ -14,18 +16,27 @@ class User(AbstractUser):
 
 
 class Client(User):
+    """
+    Model for all clients
+    """
     pass
 
 
 class Document(models.Model):
+    """
+    A Document represents a booking to be printed.
+    """
     pass
 
 
 class PrintJob(TimeStampedModel):
+    """
+    Print Jobs sent into the organisation
+    """
     charged_to = models.ForeignKey(Client)
     printer = models.CharField(max_length=256)
     copies = models.IntegerField()
     cost = models.IntegerField()
-    status_choices = (("Started","Started"),("Pending","Pending"),("Completed","Completed"))
-    status = models.CharField(choices=status_choices,max_length=256)
+    status_choices = (("Started", "Started"), ("Pending", "Pending"), ("Completed", "Completed"))
+    status = models.CharField(choices=status_choices, max_length=256)
     document = models.ForeignKey(Document)
