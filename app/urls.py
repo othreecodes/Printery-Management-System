007398a8e4c1abd0f9a6aa951b5b361b050046ba
Obi Uchenna David <daviduchenna@outlook.com>
2017-08-02 06:06:53 +0100

    user authentocatino complete

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from app import views
+
+
 urlpatterns = [
-   url(r'^$', views.index, name='home'),
+    url(r'^$', views.index, name='home'),
+    url(r'^login/$', views.auth_user, name="login")
 
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from django.views import generic
+from django.contrib.auth import authenticate,login
+from django.contrib.messages import add_message
+from django.contrib import messages
 
 def index(request):
 
     
     return render(request,"index.html")
+
+
+
+# class UserView(generic.TemplateView):
+#     template_name
+
+
+def auth_user(request):
+    if request.method == "GET":
+        return render(request,"login.html")
+    
+    username = request.POST.get("username") or None
+    password = request.POST.get("password") or None
+
+    user = authenticate(username=username,password=password)
+
+    print(user)
+    if user is not None:
+        login(request,user)
+        return render(request,"index.html")
+    else:
+        add_message(request,messages.WARNING,"INVALID USERNAME OR PASSWORD")
+        return render(request,"login.html")
+    
+
+
